9353b16feedee90e9aa54bd9b106f3ea7d16d997
fergascod <fergascod@gmail.com>
2021-05-10 09:31:57 +0200

    Initial commit

---
+++ modes.py
@@ -0,0 +1,71 @@
+
+principiant=[
+"anas platyrhynchos",
+"buteo buteo",
+"chroicocephalus ridibundus",
+"columba livia",
+"streptopelia decaocto",
+"bubo bubo",
+"turdus merula",
+"passer domesticus",
+"erithacus rubecula",
+"sylvia melanocephala",
+"hirundo rustica",
+"parus major",
+"cuculus canorus"
+]
+silvids=[
+ 'sylvia atricapilla',
+ 'sylvia borin',
+ 'sylvia hortensis',
+ 'sylvia communis',
+ 'sylvia undata',
+ 'sylvia sarda',
+ 'sylvia conspicillata',
+ 'sylvia cantillans',
+ 'sylvia melanocephala']
+fringilids=[
+ 'fringilla coelebs',
+ 'fringilla montifringilla',
+ 'coccothraustes coccothraustes',
+ 'pyrrhula pyrrhula',
+ 'bucanetes githagineus',
+ 'chloris chloris',
+ 'linaria cannabina',
+ 'loxia curvirostra',
+ 'carduelis carduelis',
+ 'carduelis citrinella',
+ 'serinus serinus',
+ 'spinus spinus']
+emberizids=[
+ 'emberiza calandra',
+ 'emberiza citrinella',
+ 'emberiza cia',
+ 'emberiza hortulana',
+ 'emberiza cirlus',
+ 'emberiza schoeniclus']
+mosquiters=[
+ 'phylloscopus sibilatrix',
+ 'phylloscopus bonelli',
+ 'phylloscopus trochilus',
+ 'phylloscopus collybita',
+ 'phylloscopus ibericus']
+
+alaudids=[
+ 'lullula arborea',
+ 'alauda arvensis',
+ 'galerida theklae',
+ 'galerida cristata',
+ 'eremophila alpestris',
+ 'calandrella brachydactyla',
+ 'melanocorypha calandra',
+ 'chersophilus duponti',
+ 'alaudala rufescens']
+modes={
+"principiants":principiant,
+"silvids":silvids,
+"fringilids":fringilids,
+"emberizids":emberizids,
+"mosquiters":mosquiters,
+"alaudids":alaudids
+}
