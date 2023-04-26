6d80b94aa6e05cbf1cd2cda920c6ca45127db77c
Fernando <fergascod@gmail.com>
2023-04-26 15:55:30 +0200

    Added listen mode and beercss

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 import flask
-from flask import request, redirect, url_for
+from flask import request, redirect, url_for, send_from_directory
 
 import random as rand
+import os
 
 from modes import modes
 from utils import loadRecordings, catalanNames
-
 
 
 # Create the application.
@@ -15,6 +15,10 @@ APP = flask.Flask(__name__)
 question=0
 max_num=0
 
+@APP.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(APP.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 @APP.route('/question/<num>/<max_num>/<game_mode>')
 def question(num, max_num, game_mode):
     targetList=modes[game_mode]
@@ -44,7 +48,20 @@ def response(num, max_num, game_mode, response, correct):
 
 @APP.route('/form')
 def form():
-    return flask.render_template('form.html', game_modes=modes.keys())
+    scientificToCatalan=catalanNames()
+    print(scientificToCatalan.values(), flush=True)
+    return flask.render_template('form.html', game_modes=modes.keys(), species=scientificToCatalan.values())
+
+@APP.route('/listen/<game_mode>')
+def listen(game_mode):
+    targetList=modes[game_mode]
+    recordings=loadRecordings()
+    scientificToCatalan=catalanNames()
+    species=rand.choice(targetList)
+    if len(recordings[species])!=0:
+        url="https:"+rand.choice(recordings[species])
+    return flask.render_template('listen.html', url=url, species=scientificToCatalan[species], game_mode=game_mode)
+
 
 @APP.route('/start_game', methods = ['POST', 'GET'])
 def start_game():
