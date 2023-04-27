09619081b3b1aacf9aa1dd4ec63972171a6cb7ec
Fernando <fergascod@gmail.com>
2023-04-27 20:27:02 +0200

    added results and css

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import flask
-from flask import request, redirect, url_for, send_from_directory
+from flask import request, redirect, url_for, send_from_directory, session
 
 import random as rand
 import os
@@ -12,15 +12,15 @@ from utils import loadRecordings, catalanNames
 
 # Create the application.
 APP = flask.Flask(__name__)
-question=0
-max_num=0
+APP.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 @APP.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(APP.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
-@APP.route('/question/<num>/<max_num>/<game_mode>')
-def question(num, max_num, game_mode):
+@APP.route('/question')
+def question():
+    num, max_num, game_mode = session["num"], session["max_num"], session["game_mode"]
     targetList=modes[game_mode]
     recordings=loadRecordings()
     scientificToCatalan=catalanNames()
@@ -38,8 +38,18 @@ def question(num, max_num, game_mode):
     else:
         return redirect(url_for("menu"))
 
-@APP.route('/response/<num>/<max_num>/<game_mode>/<response>/<correct>')
-def response(num, max_num, game_mode, response, correct):
+@APP.route('/results')
+def results():
+    correct, max_num, game_mode = session["correct"], session["max_num"], session["game_mode"]
+    return flask.render_template('results.html', 
+                                correct=correct, max_num = max_num, 
+                                game_mode=game_mode)
+
+@APP.route('/response/<response>/<correct>')
+def response(response, correct):
+    num, max_num, game_mode = session["num"], session["max_num"], session["game_mode"]
+    session["num"]+=1
+    if response==correct: session["correct"]+=1
     return flask.render_template('response.html', 
                                 num=num, max_num = max_num, 
                                 game_mode=game_mode, response=response,
@@ -70,7 +80,12 @@ def start_game():
     if request.method == 'POST':
         form_data = request.form
         max_num=form_data["num_questions"]
-        return redirect(url_for("question",num=1, max_num = max_num, game_mode=form_data["game_mode"]))
+        session["game_mode"]=form_data["game_mode"]
+        session["num"]=1
+        session["correct"]=0
+        session["max_num"]=form_data["num_questions"]
+
+        return redirect(url_for("question"))
 
 
 @APP.route('/')
